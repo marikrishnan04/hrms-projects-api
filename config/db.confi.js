5f333ebbfda6979a49939f8cdd251a058ac1cfc2
“marikrishnan04” <krishmarish777@gmail.com>
2023-11-08 20:08:37 -0800

    change

---
+++ config/db.confi.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables
+
+// const uri = process.env.MONGODB_URI;
+const uri = process.env.BASE_URL;
+
+// mongodb connect...
+function connectToDatabase() {
+  mongoose.connect(uri, {
+    // useNewUrlParser and useUnifiedTopology have been deprecated and can be removed
+  });
+
+  const db = mongoose.connection;
+
+  db.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+  db.once('open', () => {
+    console.log('Connected to MongoDB successfully!');
+  });
+}
+
+module.exports = connectToDatabase;
